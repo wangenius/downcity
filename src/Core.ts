acec7c7d23569674b5b4b12b6a915854c46953b4
wangenius <wangenius@qq.com>
2025-08-26 18:38:24 +0800

    init

---
+++ src/Core.ts
@@ -0,0 +1,6 @@
+export class Core {}
+
+export interface CoreOptions {
+  appId: string;
+  appSecret: string;
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// 导出主要的类
+export { Core } from "./Core.js";
+
+// 导出类型定义
+export type { CoreOptions } from "./Core.js";
